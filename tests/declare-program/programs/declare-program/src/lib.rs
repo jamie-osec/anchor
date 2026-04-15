f28a237d4fd5f63a222f3ab7a332bf84e448e27e
Jamie Hill-Daniel <jamie@osec.io>
2026-04-15 03:25:47 +0100

    fix(tests): update declare-program utils references to parsers

--- tests/declare-program/programs/declare-program/src/lib.rs
+++ tests/declare-program/programs/declare-program/src/lib.rs
@@ -95,23 +95,24 @@ pub mod declare_program {
     }
 
     pub fn account_utils(_ctx: Context<Utils>) -> Result<()> {
-        use external::utils::Account;
+        use external::parsers::Account;
 
         // Empty
-        if Account::try_from_bytes(&[]).is_ok() {
+        if Account::parse(&[]).is_ok() {
             return Err(ProgramError::Custom(0).into());
         }
 
         const DISC: &[u8] = external::accounts::MyAccount::DISCRIMINATOR;
 
         // Correct discriminator but invalid data
-        if Account::try_from_bytes(DISC).is_ok() {
+        if Account::parse(DISC).is_ok() {
             return Err(ProgramError::Custom(1).into());
         };
 
         // Correct discriminator and valid data
-        match Account::try_from_bytes(&[DISC, &[1, 0, 0, 0]].concat()) {
+        match Account::parse(&[DISC, &[1, 0, 0, 0]].concat()) {
             Ok(Account::MyAccount(my_account)) => require_eq!(my_account.field, 1),
+            Ok(_) => return Err(ProgramError::Custom(2).into()),
             Err(e) => return Err(e.into()),
         }
 
@@ -119,22 +120,22 @@ pub mod declare_program {
     }
 
     pub fn event_utils(_ctx: Context<Utils>) -> Result<()> {
-        use external::utils::Event;
+        use external::parsers::Event;
 
         // Empty
-        if Event::try_from_bytes(&[]).is_ok() {
+        if Event::parse(&[]).is_ok() {
             return Err(ProgramError::Custom(0).into());
         }
 
         const DISC: &[u8] = external::events::MyEvent::DISCRIMINATOR;
 
         // Correct discriminator but invalid data
-        if Event::try_from_bytes(DISC).is_ok() {
+        if Event::parse(DISC).is_ok() {
             return Err(ProgramError::Custom(1).into());
         };
 
         // Correct discriminator and valid data
-        match Event::try_from_bytes(&[DISC, &[1, 0, 0, 0]].concat()) {
+        match Event::parse(&[DISC, &[1, 0, 0, 0]].concat()) {
             Ok(Event::MyEvent(my_event)) => require_eq!(my_event.value, 1),
             Err(e) => return Err(e.into()),
         }
