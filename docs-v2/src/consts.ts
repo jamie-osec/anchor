c4f136fad0ee59587133da369ab7379157cc2321
Swaroop Maddu <swaroop@osec.io>
2026-08-12 20:22:53 +0530

    docs(v2): align docs with the current anchor v2 API (#4747)

--- docs-v2/src/consts.ts
+++ docs-v2/src/consts.ts
@@ -4,12 +4,12 @@ export const SITE: Site = {
   title: 'Anchor Docs',
   description: 'Anchor is the leading development framework for building Solana programs.',
   href: 'https://v2.anchor-lang.com',
-  author: 'solana-foundation',
+  author: 'otter-sec',
   locale: 'en-US',
 }
 
 export const SOCIAL_LINKS: SocialLink[] = [
-  { href: 'https://github.com/solana-foundation/anchor', label: 'GitHub' },
+  { href: 'https://github.com/otter-sec/anchor', label: 'GitHub' },
   { href: 'https://discord.com/invite/NHHGSXAnXk', label: 'Discord' },
 ]
 
@@ -24,9 +24,9 @@ export const ICON_MAP: IconMap = {
 }
 
 export const DOCS: DocsConfig = {
-  repoUrl: 'https://github.com/solana-foundation/anchor',
+  repoUrl: 'https://github.com/otter-sec/anchor',
   editUrlBase:
-    'https://github.com/solana-foundation/anchor/edit/anchor-next/docs-v3/src/content/docs/',
+    'https://github.com/otter-sec/anchor/edit/master/docs-v2/src/content/docs/',
   defaultEditUrl: true,
   defaultLastUpdated: true,
   defaultTableOfContents: { minDepth: 2, maxDepth: 4 },
