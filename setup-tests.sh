de8fc5023d33cf2b4e181e21c7860d011680ea41
Jamie Hill-Daniel <jamie@osec.io>
2026-09-11 15:22:29 +0100

    fix: make legacy release bump runnable

--- setup-tests.sh
+++ setup-tests.sh
@@ -13,4 +13,4 @@ cd ts/packages/spl-associated-token-account && yarn --frozen-lockfile && yarn bu
 cd ts/packages/spl-token && yarn --frozen-lockfile && yarn build:node && yarn link && cd ../../../
 cd examples/tutorial && yarn link @coral-xyz/anchor @coral-xyz/borsh && yarn --frozen-lockfile && cd ../../
 cd tests && yarn link @coral-xyz/anchor @coral-xyz/borsh @coral-xyz/spl-associated-token-account @coral-xyz/spl-token && yarn --frozen-lockfile && cd ..
-cargo install --path cli anchor-cli --locked --force --debug
+cargo +1.79.0 install --path cli anchor-cli --locked --force --debug
